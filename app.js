09b21e082bb7e4d394520ec0b6fe38643cc0db4f
Ahmed Orabi <aorabi@outlook.com>
2022-03-11 14:25:15 +0300

    app event

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
 const h = document.querySelector("h6.text-center");
 
-let i = 0
+let i = 0;
 h.addEventListener("click", function () {
-
- 
   console.log("The heading was clicked!");
 
-
   h.classList.add("text-center");
-  h.style.color = "red";
- 
+  h.style.color = "blue";
+
   h.style.fontSize = "x-large";
 
-  i +=1 ;
+  i += 1;
 
-  if(i <= 10){
-
-    h.innerHTML = `you Clicked me  ${i} times` ;
+  if (i <= 3) {
+    h.innerHTML = `you Clicked me  ${i} times`;
     h.style.backgroundColor = "black";
-  }else{
-
-    h.innerHTML = `ارحم امي العيانة` ;
+  } else {
+    h.innerHTML = `ارحم امي العيانة`;
     h.style.backgroundColor = "yellow";
-    
   }
-  
-  
 
   // window.close();
 });
