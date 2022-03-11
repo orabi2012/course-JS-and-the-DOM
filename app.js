ac1433c81b48fd4e135e4eea073236bd4c96898b
Ahmed Orabi <aorabi@outlook.com>
2022-03-11 14:38:19 +0300

    app event

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const h = document.querySelector("h6.text-center");
 
 let i = 0;
 h.addEventListener("click", function () {
-  console.log("The heading was clicked!");
+
+
+ 
 
   h.classList.add("text-center");
   h.style.color = "blue";
@@ -10,7 +12,7 @@ h.addEventListener("click", function () {
   h.style.fontSize = "x-large";
 
   i += 1;
-
+  console.log(`The heading was clicked! ${i}`);
   if (i <= 3) {
     h.innerHTML = `you Clicked me  ${i} times`;
     h.style.backgroundColor = "black";
