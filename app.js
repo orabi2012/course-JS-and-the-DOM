fdd95fbefdd7c85dd532edddb4c181e48c889b46
Ahmed Orabi <aorabi@outlook.com>
2022-03-12 01:52:20 +0300

    event

--- app.js
+++ app.js
@@ -47,9 +47,11 @@ thirdLink.addEventListener("click", function (event) {
   console.log("Look, ma! We didn't navigate to a new page!");
 });
 
-const myCustomDiv = document.createElement("div");
+
 
 function respondToTheClick(event) {
+
+
   if (event.target.nodeName === "P") {
     console.log(`A ${event.target.nodeName} was clicked. ${event.target.textContent}`);
   }else{
@@ -58,17 +60,59 @@ function respondToTheClick(event) {
   }
 }
 
-for (let i = 1; i <= 10; i++) {
+performance.now()
+
+const myCustomDiv = document.createElement("div");
+const fragment = document.createDocumentFragment();
+
+const startingTime = performance.now();
+
+for (let i = 1; i <= 1; i++) {
   const newElement = document.createElement("p");
   newElement.style.border = "thin solid #000000";
 
   newElement.style.backgroundColor = "green";
+  newElement.style.color = "white"
   newElement.textContent = "This is paragraph number " + i + "\n";
+ // newElement.addEventListener("click", respondToTheClick);
 
-  myCustomDiv.appendChild(newElement);
+ myCustomDiv.appendChild(newElement);
+
+  
+
 }
 
-myCustomDiv.addEventListener("click", respondToTheClick);
 
-myCustomDiv.style.border = "thick solid #0000FF";
+
+//myCustomDiv.style.border = "thick solid #0000FF";
+//document.body.appendChild(myCustomDiv);
 document.body.appendChild(myCustomDiv);
+
+const endingTime = performance.now();
+console.log('This code took ' + (Math.round(endingTime - startingTime) ) + ' milliseconds.');
+
+let count = 1
+
+function generateParagraphs() {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 1; i <= 500; i++) {
+        const newElement = document.createElement('p');
+        newElement.textContent = 'This is paragraph number ' + count;
+        console.log(`count = ${count}`);
+        count = count + 1;
+
+        fragment.appendChild(newElement);
+    }
+
+    document.body.appendChild(fragment);
+
+    
+    if (count > 1000) {
+        setTimeout(generateParagraphs, 0);
+    }
+}
+
+setTimeout(generateParagraphs, 1000);
+
+//generateParagraphs();
